bba297ccb99676f969fab8df285bd03c76c81ee9
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 03:58:52 +0200

    subprocess Modul

---
+++ test-os-system.py
@@ -0,0 +1,20 @@
+import os
+
+##show  the current folder and files directory
+#os.system('ls')
+
+# #show current directory
+# os.system('pwd')
+
+# #shut Down pc
+# os.system('shutdown /s')
+
+
+# #restart
+# os.system('shutdown /r')
+
+
+
+import subprocess
+subprocess.call(['shutdown','/r'])
+
