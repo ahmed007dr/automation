6a5d87b43dfca6e1e75b74819a27abe1e04a90e3
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 02:52:45 +0200

    loop in hard desk

--- remove_empty.py
+++ remove_empty.py
@@ -3,6 +3,11 @@ import os
 #dir name = folders
 #file name = files.txt
 
-for dirpath , dirname , filenames in os.walk('.'):
-    if not dirname and not filenames:
-        os.rmdir(dirpath)
+# for dirpath , dirname , filenames in os.walk('.'):
+#     if not dirname and not filenames:
+#         os.rmdir(dirpath)
+
+#loop in hard desk
+
+for dirpath , dirname , filenames in os.walk('c:'):
+    print(dirpath)
