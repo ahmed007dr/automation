c57c25498a7e43b3a0428c9bf87e50e4f9666936
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 03:47:53 +0200

    keybord pyautogui

---
+++ PyAutoGui.py
@@ -0,0 +1,23 @@
+import os
+
+#pip install PyAutoGUI
+import pyautogui
+#print(dir(pyautogui))
+
+
+# #size ur screen
+# print(pyautogui.size())
+
+# #x & y        ur mouse
+# print(pyautogui.position())
+
+# #to move ur mouse
+# pyautogui.moveTo(700,700)
+
+#keybord  ( word + secnd)
+pyautogui.typewrite('ahmed mohamed abdelahmed el said embabi',4)
+
+pyautogui.hotkey('ctrl','space')
+
+pyautogui.screenshot('test.png')
+
